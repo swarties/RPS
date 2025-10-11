ffc28dce0d850092818b461f58518598445186a1
swarties <76826079+swarties@users.noreply.github.com>
2025-10-11 18:23:26 -0400

    Finished UI

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-function getHumanChoice() {
-    return prompt("Choose what you want to play (Rock, Paper or Scissors)");
-}
+// function getHumanChoice() {
+//     return prompt("Choose what you want to play (Rock, Paper or Scissors)");
+// }
+
+
 
 function getComputerChoice() {
     let value = Math.random();
@@ -13,56 +15,93 @@ function getComputerChoice() {
     }
     
 }
+const btns = document.querySelectorAll(".btn");
+btns.forEach(btn => {
+  btn.onclick = function buttonClicked() {
+    return btn.textContent.slice(3);
+    
+  };
+  btn.onclick = () => {
+    const response = document.querySelector('#datH2');
+    response.textContent = "...";
+    response.textContent = playRound(btn.textContent.slice(3),getComputerChoice());
+    let textHumanScore = document.querySelector('#you');
+    textHumanScore.textContent = "You : " + HumanScore;
+
+    let textRobotScore = document.querySelector('#robot');
+    textRobotScore.textContent = "Robot : " + ComputerScore;
+
+    if (HumanScore == 5) {
+        response.textContent = "You Win! Score will reset!"
+        setTimeout(function() {
+        HumanScore = 0;
+        ComputerScore = 0;
+        textRobotScore.textContent = "Robot : " + ComputerScore;
+        textHumanScore.textContent = "You : " + HumanScore;
+    }, 250);
+    } else if (ComputerScore == 5) {
+        response.textContent = "You lose :(... Score will reset!"
+        setTimeout(() => {
+        HumanScore = 0;
+        ComputerScore = 0;
+    }, 250);
+    }
+  };
+});
+
+
+
 
 let HumanScore = 0;
 let ComputerScore = 0;
 
 function playRound(HumanChoice, ComputerChoice) {
-    let FixedHumanChoice = HumanChoice[0].toUpperCase() + HumanChoice.slice(1).toLowerCase();
-    if (FixedHumanChoice == ComputerChoice) {
+    if (HumanChoice == ComputerChoice) {
 
         return "Draw! You both chose " + ComputerChoice;
 
-    } else if (FixedHumanChoice == "Rock") {
+    } else if (HumanChoice == "Rock") {
         if (ComputerChoice == "Paper") {
             ComputerScore++;
-            return "You lose! " + ComputerChoice + " beats " + FixedHumanChoice + ".";
+            return "You lose! " + ComputerChoice + " beats " + HumanChoice + ".";
         } else if (ComputerChoice == "Scissors") {
             HumanScore++;
-            return "You win! " + FixedHumanChoice + " beats " + ComputerChoice + ".";
+            return "You win! " + HumanChoice + " beats " + ComputerChoice + ".";
         }
-    } else if (FixedHumanChoice ==  "Paper") {
+    } else if (HumanChoice ==  "Paper") {
         if (ComputerChoice == "Scissors") {
             ComputerScore++;
-            return "You lose! " + ComputerChoice + " beats " + FixedHumanChoice + ".";
+            return "You lose! " + ComputerChoice + " beats " + HumanChoice + ".";
         } else if (ComputerChoice == "Rock") {
             HumanScore++;
-            return "You win! " + FixedHumanChoice + " beats " + ComputerChoice + ".";
+            return "You win! " + HumanChoice + " beats " + ComputerChoice + ".";
         }
-    } else if (FixedHumanChoice == "Scissors") {
+    } else if (HumanChoice == "Scissors") {
         if (ComputerChoice == "Rock") {
             ComputerScore++;
-            return "You lose! " + ComputerChoice + " beats " + FixedHumanChoice + ".";
+            return "You lose! " + ComputerChoice + " beats " + HumanChoice + ".";
         } else if (ComputerChoice == "Paper") {
             HumanScore++;
-            return "You win! " + FixedHumanChoice + " beats " + ComputerChoice + ".";
+            return "You win! " + HumanChoice + " beats " + ComputerChoice + ".";
         }
     }
     return "There's an error somewhere :(";
 }
 
 
-for (let i = 1; i <= 5; i++) {
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-    alert(playRound(humanSelection, computerSelection))
-    alert("The Current Score is :\n" + "Human Score : " + HumanScore + "\n" + "Computer Score : " + ComputerScore);
-}
 
-if (HumanScore == ComputerScore) {
-    alert("Draw! Refresh to play again!")
-} else if (HumanScore < ComputerScore) {
-    alert("You lose! Refresh to play again!")
-} else if (HumanScore > ComputerScore) {
-    alert("You win! Refresh to play again!")
-}
+
+// for (let i = 1; i <= 5; i++) {
+//     const humanSelection = getHumanChoice();
+//     const computerSelection = getComputerChoice();
+//     alert(playRound(humanSelection, computerSelection))
+//     alert("The Current Score is :\n" + "Human Score : " + HumanScore + "\n" + "Computer Score : " + ComputerScore);
+// }
+
+// if (HumanScore == ComputerScore) {
+//     alert("Draw! Refresh to play again!")
+// } else if (HumanScore < ComputerScore) {
+//     alert("You lose! Refresh to play again!")
+// } else if (HumanScore > ComputerScore) {
+//     alert("You win! Refresh to play again!")
+// }
